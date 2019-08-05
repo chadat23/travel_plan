352ddd629dcfe7bfc9d6973e8f371fcc76dffc34
DeRosier <cderosier@nps.gov>
2019-08-05 14:20:26 -0700

    Added locations and entry page notes

--- travel_plan/bin/basic_inserts.py
+++ travel_plan/bin/basic_inserts.py
@@ -28,32 +28,28 @@ def insert_users():
     u = User()
     u.name = 'Jane Dow'
     u.email = 'chad.derosier+a@gmail.com'
-
     session.add(u)
 
     u = User()
     u.name = 'John Dow'
     u.email = 'chad.derosier+s@gmail.com'
-
     session.add(u)
 
     u = User()
     u.name = 'Darth Vader'
     u.email = 'chad.derosier+d@gmail.com'
-
     session.add(u)
 
     u = User()
     u.name = 'Roger Rabbit'
     u.email = 'chad.derosier+f@gmail.com'
-
     session.add(u)
 
     u = User()
     u.name = 'Rocky Balboa'
     u.email = 'chad.derosier+g@gmail.com'
-
     session.add(u)
+
     session.commit()
 
 
@@ -62,32 +58,53 @@ def insert_locations():
     session = db_session.create_session_existing()
 
     loc = Location()
-    loc.name = "Ten Lakes TH"
-    loc.latitude = 37.852321
-    loc.longitude = -119.575861
-
+    loc.name = "Happy Isles TH"
+    loc.latitude = 37.732555
+    loc.longitude = -119.557803
     session.add(loc)
-
+    
     loc = Location()
-    loc.name = "Ten Lakes Basin"
-    loc.latitude = 37.899158
-    loc.longitude = -119.522609
-
-    session.add(loc)
-
-    loc = Location()
-    loc.name = "May Lake TH"
-    loc.latitude = 37.832687
-    loc.longitude = -119.490761
-
+    loc.name = "LYV"
+    loc.latitude = 37.733023
+    loc.longitude = -119.514508
     session.add(loc)
 
     loc = Location()
     loc.name = "May Lake HSC"
     loc.latitude = 37.844617
     loc.longitude = -119.491018
-
     session.add(loc)
+
+    loc = Location()
+    loc.name = "May Lake TH"
+    loc.latitude = 37.832687
+    loc.longitude = -119.490761
+    session.add(loc)
+    
+    loc = Location()
+    loc.name = "Ten Lakes Basin"
+    loc.latitude = 37.899158
+    loc.longitude = -119.522609
+    session.add(loc)
+
+    loc = Location()
+    loc.name = "Ten Lakes TH"
+    loc.latitude = 37.852321
+    loc.longitude = -119.575861
+    session.add(loc)
+
+    loc = Location()
+    loc.name = "Sunrise Lakes"
+    loc.latitude = 37.805904
+    loc.longitude = -119.448250
+    session.add(loc)
+    
+    loc = Location()
+    loc.name = "Sunrise Lakes TH"
+    loc.latitude = 37.826962
+    loc.longitude = -119.468687
+    session.add(loc)    
+
     session.commit()
 
 
