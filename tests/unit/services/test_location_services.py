03b1b689954378d0ff1dea6ba16512c387696874
Chad DeRosier <chad.derosier@gmail.com>
2019-09-09 09:34:52 -0700

    location_services fully tested

--- tests/unit/services/test_location_services.py
+++ tests/unit/services/test_location_services.py
@@ -1,36 +1,6 @@
 from sqlalchemy.orm import Session
 
 
-def test_location_services_add_location_returns_location(db_session_wo_info: Session):
-    from travel_plan.models.locations import Location
-    from travel_plan.services.location_services import add_location
-
-    name = "LYV"
-    latitude = 37.733023
-    longitude = -119.514508
-
-    location = add_location(name, latitude, longitude)
-
-    assert isinstance(location, Location)
-    assert name == location.name
-    assert latitude == location.latitude
-    assert longitude == location.longitude
-
-
-def test_location_services_get_location_success(db_session_w_info):
-    from travel_plan.models.locations import Location
-    from travel_plan.services import location_services
-
-    locations, users, colors, cars = db_session_w_info
-
-    for location in locations:
-        loc = location_services.get_location(location['name'])
-        assert isinstance(loc, Location)
-        assert loc.name == location['name']
-        assert loc.latitude == location['latitude']
-        assert loc.longitude == location['longitude']
-
-
 def test_location_services_get_all_success(db_session_w_info):
     from travel_plan.services import location_services
 
@@ -65,3 +35,33 @@ def test_location_services_get_id_from_name_success(db_session_w_info):
     expected_location = 2
 
     assert expected_location == actual_locaction
+
+
+def test_location_services_add_location_returns_location(db_session_wo_info: Session):
+    from travel_plan.models.locations import Location
+    from travel_plan.services.location_services import add_location
+
+    name = "LYV"
+    latitude = 37.733023
+    longitude = -119.514508
+
+    location = add_location(name, latitude, longitude)
+
+    assert isinstance(location, Location)
+    assert name == location.name
+    assert latitude == location.latitude
+    assert longitude == location.longitude
+
+
+def test_location_services_get_location_success(db_session_w_info):
+    from travel_plan.models.locations import Location
+    from travel_plan.services import location_services
+
+    locations, users, colors, cars = db_session_w_info
+
+    for location in locations:
+        loc = location_services.get_location(location['name'])
+        assert isinstance(loc, Location)
+        assert loc.name == location['name']
+        assert loc.latitude == location['latitude']
+        assert loc.longitude == location['longitude']
