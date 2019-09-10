edb33252251a6bba20ced20ec01aa28c22b8c478
Chad DeRosier <chad.derosier@gmail.com>
2019-09-09 22:26:10 -0700

    Strating to move conftest.py stuff

--- tests/unit/views/test_travel_views.py
+++ tests/unit/views/test_travel_views.py
@@ -6,13 +6,6 @@ from tests.test_client import flask_app
 import unittest.mock
 
 
-def test_(db_session_w_info, form_data, initialized_users, initialized_locations, initialized_cars,
-          initialized_colors):
-    from travel_plan.views.travel_views import entry_post
-
-    with flask_app.test_request_context(path='/travel/entry', data=form_data):
-        resp: Response = entry_post()
-
 def test_travel_view_entry_post_success(db_session_w_info, form_data,
                                         initialized_users, initialized_locations, initialized_cars,
                                         initialized_colors):
