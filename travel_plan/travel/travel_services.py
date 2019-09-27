7674cb6d5b2d4a06a897a1bbf839d6c205b2b31d
Chad DeRosier <chad.derosier@gmail.com>
2019-09-26 17:40:07 -0700

    Merged in department and note addition

--- travel_plan/travel/travel_services.py
+++ travel_plan/travel/travel_services.py
@@ -96,7 +96,6 @@ def create_plan(start_date: str, entry_point: str, end_date: str, exit_point: st
     for day in day_plans:
         day.travel = travel
         db.session.add(day)
-    # db.session.commit()
     for file in files:
         # TODO: this could be better
         if not travel_file_services.is_present(file.name):
