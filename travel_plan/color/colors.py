661144e82fb350e097a9fb63691b649f7098972e
DeRosier <CDeRosier@nps.gov>
2019-09-29 10:20:06 -0700

    anotated color.py

--- travel_plan/color/colors.py
+++ travel_plan/color/colors.py
@@ -4,12 +4,28 @@ from travel_plan import db
 
 
 class Color(db.Model):
+    '''
+    An object respresenting an items color.
+
+    :prop id: the database key
+    :prop name: the name of the color
+    '''
+
     __tablename__ = 'colors'
 
     id: str = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String, nullable=False)
 
     def __init__(self, name: str):
+        '''
+        Initialize a color object.
+
+        All colors are to have "title" capitalization.
+        (This Is An Example Of Title Capitalization)
+
+        :prop name: the name of the color
+        :type name: str
+        '''
         self.name = name.lower().strip().title()
 
     def __lt__(self, other):
